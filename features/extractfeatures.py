b3f71497bf85d9caf205ff68330c812ee56cf4bc
Diane Duros <dduros1@gmail.com>
2015-01-10 12:18:54 -0800

    feature representation finished. clean up code and create models todo

--- features/extractfeatures.py
+++ features/extractfeatures.py
@@ -3,31 +3,84 @@ import subprocess
 import argparse
 import os
 
-looptool = '../pin/source/tools/MyPinTool/obj-intel64/simpleloop.so'
+pindir = '/home/d/Documents/shiny-thesis/pin'
+looptool = pindir+'/source/tools/MyPinTool/obj-intel64/simpleloop.so'
+categorytool = pindir+'/source/tools/MyPinTool/obj-intel64/categorycount.so'
+instructiontool = pindir+'/source/tools/MyPinTool/obj-intel64/arithinscount.so'
+pin = pindir+'/pin.sh'
+fileDirectory = './featurefiles/'
+debug = False
 
 def processDirectory(dirName):
 	for root, dirs, files in os.walk(dirName):
 		for currFile in files:
 			print currFile
-			#processFile(currFile)
+			processFile(currFile, dirName+currFile)
 	return
 
-def processFile(fileName):
-	countLoops(fileName)
-	countArithmeticInstructions(fileName)
+def processFile(fileName, filePath):
+	loopfeatures = countLoops(filePath)
+	instrfeatures = countInstructions(filePath)
+	categoryfeatures = countCategories(filePath)
 
-	return
+	ifile = open(fileDirectory+fileName+'_ins.feature', 'w+')
+	cfile = open(fileDirectory+fileName+'_cat.feature', 'w+')
+	
+	for feat in instrfeatures:
+		ifile.write(str(feat)+'\n')
+	for feat in categoryfeatures:
+		cfile.write(str(feat)+'\n')
+	#for feat in loopfeatures:
+	#	ifile.write(str(feat))
+	#	cfile.write(str(feat))
 
 
-def countArithmeticInstructions(currFile):
-	#currently just counting instructions. need to write the damn pintool
-	return
+def countCategories(currFile):
+	categories = [0]*61
+
+	command = [pin, '-t', categorytool, '--', currFile]
+	try:
+		subprocess.call(command)
+	except Exception:
+		print 'Error in processing categories for', currFile
+		return
+
+	#writes to categorycount.out file, need to open and process file
+	with open('categorycount.out', 'r') as f:
+		for line in f:
+			eles = line.split('\t')
+			if int(eles[0]) <= 60:
+				categories[int(eles[0])] = eles[2]
+
+	return categories
+
+#TODO i think that everything is index one, soooo index 0 shouldn't be included
+#TODO change if statements to exception handling
+def countInstructions(currFile):
+	instructions = [0]*1134
+
+	command = [pin, '-t', instructiontool, '--', currFile]
+	try:
+		subprocess.call(command)
+	except Exception:
+		print 'Error in processing instructions for', currFile
+		return
+
+	#writes to instrtypecount.out
+	with open('instrtypecount.out', 'r') as f:
+		for line in f:
+			eles = line.split('\t')
+			if int(eles[0]) <= 1133:
+				instructions[int(eles[0])] = eles[1]
+	return instructions
 
 def countLoops(currFile):
-	import re
-	#./pin.sh -t source/tools/MyPinTool/obj-intel64/simpleloop.so -- ../binaries/aestest_gcc_fno-pic_Og > looptest3.out
-	command = ['./../pin/pin.sh', '-t', looptool, '--', currFile]
-	output = subprocess.check_output(command)
+	command = [pin, '-t', looptool, '--', currFile]
+	try:
+		output = subprocess.check_output(command)
+	except Exception:
+		print 'Error in processing loops for', currFile
+		return
 
 	output = output.split('\n')
 	output = output[2:len(output)-1]
@@ -38,20 +91,54 @@ def countLoops(currFile):
 		eles = line.split('\t')
 		if int(eles[1]) > 2:
 			keep.append(eles)	
-	for item in keep:
-		print item
 	
-	#TODO currently keeping instruction and number of loops. Need to figure out a way to represent that as a feature
 
+	if debug:
+		f = open('loopfeatures.test', 'a+')
+		for item in keep:
+			f.write(str(item[1])+'\t' + str(item[2])+'\n')
+		f.close()
+	
+	return parseLoopFeature(keep)
 
-	return
+def parseLoopFeature(keep):
+	features = [0]*8
+	for ele in keep:
+		count = ele[1]
+		ins = ele[2]
+		cmd = ins.split()[0]
+		try:
+			features[loopidx(cmd)] += int(count)
+		except IndexError:
+			continue		#not a feature we're keeping
+	print features
+	return features
+			
+
+def loopidx(x):
+	return {
+		'push':0,
+		'jmp':1,
+		'repne':2,
+		'add':3,
+		'lea':4,
+		'not':5,
+		'and':6,
+		'jz':7,
+	}.get(x, -1)	#-1, not a feature command
 
 def main():
+	global debug
+
 	#setup arguments
 	parser = argparse.ArgumentParser(description='Feature Extraction using Pin')
 	parser.add_argument('-d', type=str, help='A directory containing files to process')
 	parser.add_argument('-f', type=str, help='A file to process')
+	parser.add_argument('-debug', action='store_true')
 	args = parser.parse_args()
+
+	if (args.debug):
+		debug = True
 
 	if (args.d == None and args.f == None):
 		print 'Please specify file or directory of files to process'
