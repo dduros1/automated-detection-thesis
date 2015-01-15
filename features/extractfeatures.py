28450da1ab95bc8b7f67f203faf5d80ddd6831d4
Diane Duros <dduros1@gmail.com>
2015-01-14 23:38:13 -0800

    updated extractfeatures to output into ins and cat dirs

--- features/extractfeatures.py
+++ features/extractfeatures.py
@@ -12,6 +12,7 @@ pin = pindir+'/pin.sh'
 fileDirectory = thesisdir+'/features/featurefiles/'
 debug = False
 
+
 def processDirectory(dirName):
 	for root, dirs, files in os.walk(dirName):
 		for currFile in files:
@@ -20,139 +21,147 @@ def processDirectory(dirName):
 	return
 
 def processFile(fileName, filePath):
-	if fileName == None:
-		fileName = filePath.split('/')[-1]
+    if fileName == None:
+        fileName = filePath.split('/')[-1]
 
-	print 'processing', fileName, '...'
-	loopfeatures = countLoops(filePath)
-	instrfeatures = countInstructions(filePath)
-	categoryfeatures = countCategories(filePath)
+    print 'processing', fileName, '...'
+    loopfeatures = countLoops(filePath)
+    instrfeatures = countInstructions(filePath)
+    categoryfeatures = countCategories(filePath)
 
-	ifile = open(fileDirectory+fileName+'_ins.feature', 'w+')
-	cfile = open(fileDirectory+fileName+'_cat.feature', 'w+')
+#print instrfeatures
+
+    ifile = open(fileDirectory+'ins/'+fileName+'_ins.feature', 'w+')
+    cfile = open(fileDirectory+'cat/'+fileName+'_cat.feature', 'w+')
 	
-	for feat in instrfeatures:
-		ifile.write(str(feat)+'\n')
-	for feat in categoryfeatures:
-		cfile.write(str(feat)+'\n')
-	#for feat in loopfeatures:
-	#	ifile.write(str(feat))
-	#	cfile.write(str(feat))
+    for feat in instrfeatures:
+        ifile.write(str(feat).rstrip()+'\n')
+    for feat in categoryfeatures:
+        cfile.write(str(feat).rstrip()+'\n')
+    for feat in loopfeatures:
+        ifile.write(str(feat).rstrip()+'\n')
+        cfile.write(str(feat).rstrip()+'\n')
 
 
 def countCategories(currFile):
-	categories = [0]*61
+    categories = [0]*61
 
-	command = [pin, '-t', categorytool, '--', currFile]
-	try:
-		subprocess.call(command)
-	except Exception:
-		print 'Error in processing categories for', currFile
-		return
+    command = [pin, '-t', categorytool, '--', currFile]
+    try:
+        subprocess.call(command)
+    except Exception:
+        print 'Error in processing categories for', currFile
+        return categories
 
 	#writes to categorycount.out file, need to open and process file
-	with open('categorycount.out', 'r') as f:
-		for line in f:
-			eles = line.split('\t')
-			if int(eles[0]) <= 60:
-				categories[int(eles[0])] = eles[2]
+    with open('categorycount.out', 'r') as f:
+        for line in f:
+            eles = line.split('\t')
+            if int(eles[0]) <= 60:
+                categories[int(eles[0])] = eles[2]
 
-	print 'category features extracted'
-	return categories
+    print 'category features extracted'
+    return categories
 
 #TODO i think that everything is index one, soooo index 0 shouldn't be included
 #TODO change if statements to exception handling
 def countInstructions(currFile):
-	instructions = [0]*1134
+    instructions = [0]*1134
 
-	command = [pin, '-t', instructiontool, '--', currFile]
-	try:
-		subprocess.call(command)
-	except Exception:
-		print 'Error in processing instructions for', currFile
-		return
+    command = [pin, '-t', instructiontool, '--', currFile]
+    try:
+        subprocess.call(command)
+    except Exception:
+        print 'Error in processing instructions for', currFile
+        return instructions
 
 	#writes to instrtypecount.out
-	with open('instrtypecount.out', 'r') as f:
-		for line in f:
-			eles = line.split('\t')
-			if int(eles[0]) <= 1133:
-				instructions[int(eles[0])] = eles[1]
-	print 'Instruction features extracted'	
-	return instructions
+    with open('instrtypecount.out', 'r') as f:
+        for line in f:
+            eles = line.split('\t')
+#print int(eles[0]), eles[2]
+            if int(eles[0]) <= 1133:
+                instructions[int(eles[0])] = eles[2]
+    print 'Instruction features extracted'	
+    return instructions
 
 def countLoops(currFile):
-	command = [pin, '-t', looptool, '--', currFile]
-	try:
-		output = subprocess.check_output(command)
-	except Exception:
-		print 'Error in processing loops for', currFile
-		return
+    command = [pin, '-t', looptool, '--', currFile]
+    try:
+        output = subprocess.check_output(command)
+    except Exception:
+        print 'Error in processing loops for', currFile
+        return [0]*8
 
-	output = output.split('\n')
-	output = output[2:len(output)-1]
+    output = output.split('\n')
+    output = output[2:len(output)-1]
 
-	#only keep loops with more than one iteration
-	keep = []
-	for line in output:
-		eles = line.split('\t')
-		if int(eles[1]) > 2:
-			keep.append(eles)	
+    #only keep loops with more than one iteration
+    keep = []
+    for line in output:
+        eles = line.split('\t')
+        if int(eles[1]) > 2:
+            keep.append(eles)	
 	
 
-	if debug:
-		f = open('loopfeatures.test', 'a+')
-		for item in keep:
-			f.write(str(item[1])+'\t' + str(item[2])+'\n')
-		f.close()
+    if debug:
+        f = open('loopfeatures.test', 'a+')
+        for item in keep:
+            f.write(str(item[1])+'\t' + str(item[2])+'\n')
+        f.close()
 	
-	print 'Loop features extracted'
-	return parseLoopFeature(keep)
+    print 'Loop features extracted'
+    return parseLoopFeature(keep)
 
 def parseLoopFeature(keep):
-	features = [0]*8
-	for ele in keep:
-		count = ele[1]
-		ins = ele[2]
-		cmd = ins.split()[0]
-		try:
-			features[loopidx(cmd)] += int(count)
-		except IndexError:
-			continue		#not a feature we're keeping
-	return features
+    features = [0]*8
+    for ele in keep:
+        count = ele[1]
+        ins = ele[2]
+        cmd = ins.split()[0]
+        try:
+            features[loopidx(cmd)] += int(count)
+        except IndexError:
+            continue		#not a feature we're keeping
+    return features
 			
 
 def loopidx(x):
-	return {
-		'push':0,
-		'jmp':1,
-		'repne':2,
-		'add':3,
-		'lea':4,
-		'not':5,
-		'and':6,
-		'jz':7,
-	}.get(x, -1)	#-1, not a feature command
+    return {
+        'push':0,
+        'jmp':1,
+        'repne':2,
+        'add':3,
+        'lea':4,
+        'not':5,
+        'and':6,
+        'jz':7,
+    }.get(x, -1)	#-1, not a feature command
 
 def main():
-	global debug
+    global debug
+    global fileDirectory
 
 	#setup arguments
-	parser = argparse.ArgumentParser(description='Feature Extraction using Pin')
-	parser.add_argument('-d', type=str, help='A directory containing files to process')
-	parser.add_argument('-f', type=str, help='A file to process')
-	parser.add_argument('-debug', action='store_true')
-	args = parser.parse_args()
+    parser = argparse.ArgumentParser(description='Feature Extraction using Pin')
+    parser.add_argument('-o', type=str, help='Output directory for feature files')
+    parser.add_argument('-d', type=str, help='A directory containing files to process')
+    parser.add_argument('-f', type=str, help='A file to process')
+    parser.add_argument('-debug', action='store_true')
+    args = parser.parse_args()
 
-	if (args.debug):
-		debug = True
+    if (args.debug):
+        debug = True
 
-	if (args.d == None and args.f == None):
-		print 'Please specify file or directory of files to process'
-	elif (args.d != None):
-		processDirectory(args.d)
-	elif (args.f != None):
-		processFile(None, args.f)
+    if (args.o != None):
+        fileDirectory=args.o
+
+    if (args.d == None and args.f == None):
+        print 'Please specify file or directory of files to process'
+    elif (args.d != None):
+        processDirectory(args.d)
+    elif (args.f != None):
+        processFile(None, args.f)
 	
 
 main()
